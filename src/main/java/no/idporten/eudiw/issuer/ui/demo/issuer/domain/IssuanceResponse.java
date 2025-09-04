2366c6fe98394a0b15e1c53c778092f9d120fca7
Randi Øyri <randi.oyri@digdir.no>
2025-09-04 14:06:24 +0200

    EUW-477: ny response med issuer-transaction

--- src/main/java/no/idporten/eudiw/issuer/ui/demo/issuer/domain/IssuanceResponse.java
+++ src/main/java/no/idporten/eudiw/issuer/ui/demo/issuer/domain/IssuanceResponse.java
@@ -2,5 +2,5 @@ package no.idporten.eudiw.issuer.ui.demo.issuer.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record IssuanceResponse(@JsonProperty("credential_offer") CredentialOffer credentialOffer) {
+public record IssuanceResponse(@JsonProperty("credential_offer") CredentialOffer credentialOffer, @JsonProperty("issuer_transaction_id") String issuerTransactionId) {
 }
